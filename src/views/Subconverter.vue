8ac5ea23f382b0b0270f24ac3e956c4fa44b112b
wanyouqiyuan <fengwangke689@gmail.com>
2025-09-27 01:18:56 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -26,7 +26,7 @@
                   <el-option v-for="(v, k) in options.clientTypes" :key="k" :label="k" :value="v"></el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item label="后端地址:">
+              <el-form-item label="后端地址:https://subapi.ncozee.com">
                 <el-select v-model="form.customBackend" allow-create filterable @change="selectChanged"
                   placeholder="可输入自己的后端" style="width: 100%">
                   <el-option v-for="(v, k) in options.customBackend" :key="k" :label="k" :value="v"></el-option>
@@ -1310,3 +1310,4 @@ export default {
   }
 };
 </script>
+
